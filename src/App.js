4ce5c018d1a4cd3a6c21f4b550af48887420b1a5
Khang Ho <themarinect@gmail.com>
2021-05-03 18:26:29 -0400

    Updated App.js with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id='colorlib-page'>
+
+        <div id='container-wrap'>
+
+          <Sidebar></Sidebar>
+
+          <div id='colorlib-main'>
+            <Home></Home>
+            <About></About>
+            <Projects></Projects>
+            <Experience></Experience>
+          </div>
+
+        </div>
+
+      </div>
   );
 }
 
