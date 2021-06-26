422c1c7aa1afff3c884f72fd70095b6870cb0f43
Khang Ho <themarinect@gmail.com>
2021-06-26 16:52:31 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ export default class Home extends Component{
                                         <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                             <div className="slider-text-inner js-fullheight">
                                                 <div className="desc">
-                                                    <h1>Hi! <br />I'm Khang Ho</h1>
-                                                    <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
+                                                    <h1 style={{color:"white"}}>Hi! <br />I'm Khang Ho</h1>
+                                                    <p><a style={{color:"white"}} className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -29,8 +29,8 @@ export default class Home extends Component{
                                         <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                             <div className="slider-text-inner">
                                                 <div className="desc">
-                                                    <h1>I love building<br /> THINGS !!</h1>
-                                                    <p><a className="btn btn-primary btn-learn" href="https://github.com/themarinect" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                                                    <h1 style={{color:"white"}}>I love building<br /> THINGS !!</h1>
+                                                    <p><a style={{color:"white"}} className="btn btn-primary btn-learn" href="https://github.com/themarinect" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -44,8 +44,8 @@ export default class Home extends Component{
                                         <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                             <div className="slider-text-inner">
                                                 <div className="desc">
-                                                    <h1>Check out <br/>My LinkedIn Profile ... </h1>
-                                                    <p><a className="btn btn-primary btn-learn" href="https://www.linkedin.com/in/khang-ho-193246131/" target="_blank" rel="noopener noreferrer">View LinkedIn <i className="icon-book" /></a></p>
+                                                    <h1 style={{color:"white"}}>Check out <br/>My LinkedIn Profile ... </h1>
+                                                    <p><a style={{color:"white"}} className="btn btn-primary btn-learn" href="https://www.linkedin.com/in/khang-ho-193246131/" target="_blank" rel="noopener noreferrer">View LinkedIn <i className="icon-book" /></a></p>
                                                 </div>
                                             </div>
                                         </div>
